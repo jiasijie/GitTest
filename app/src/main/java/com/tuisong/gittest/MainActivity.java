6ed105d6b589685e9fc42597847c3d52453c18af
jiasijie <15712965899@163.com>
2019-09-03 15:20:38 +0800

    增加控件

--- app/src/main/java/com/tuisong/gittest/MainActivity.java
+++ app/src/main/java/com/tuisong/gittest/MainActivity.java
@@ -2,9 +2,11 @@ package com.tuisong.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mTvContent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
